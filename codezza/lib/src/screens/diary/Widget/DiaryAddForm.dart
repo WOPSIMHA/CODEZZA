bc7d14e17568c8f7ea6900e1bc415bb8ccd82ddd
WOPSIMHA <wisdom@c21soft.com>
2021-08-09 12:32:31 +0900

    imagepicker list 작업 중

--- codezza/lib/src/screens/diary/Widget/DiaryAddForm.dart
+++ codezza/lib/src/screens/diary/Widget/DiaryAddForm.dart
@@ -17,12 +17,14 @@ class DiaryAddForm extends StatefulWidget {
   final state = _DiaryAddFormState();
   final OnDelete? onDelete;
   final String? title;
+  final int? index;
 
   DiaryAddForm({
     Key? key,
     this.diary,
     this.onDelete,
     this.title,
+    this.index,
   }) : super(key: key);
 
   @override
@@ -38,7 +40,12 @@ class _DiaryAddFormState extends State<DiaryAddForm> {
   /** 이미지 업로드 */
   List<PickedFile>? _imageFileList;
   set _imageFile(PickedFile? value) {
-    _imageFileList = value == null ? null : [value];
+    if (value == null) {
+      return;
+    } else {
+      _imageFileList?.add(value);
+    }
+    // _imageFileList = value == null ? null : [value];
   }
 
   dynamic _pickImageError;
@@ -177,167 +184,20 @@ class _DiaryAddFormState extends State<DiaryAddForm> {
       });
     }
   }
-}
 
-class _DiaryPhtoPick extends State<MyHomePage> {
-  List<PickedFile>? _imageFileList;
-
-  set _imageFile(PickedFile? value) {
-    _imageFileList = value == null ? null : [value];
-  }
-
-  dynamic _pickImageError;
-
-  String? _retrieveDataError;
-
-  final ImagePicker _picker = ImagePicker();
-
-  void _onImageButtonPressed(ImageSource source,
-      {BuildContext? context}) async {
-    try {
-      final pickedFile = await _picker.getImage(
-        source: source,
-      );
-      setState(() {
-        _imageFile = pickedFile;
-      });
-    } catch (e) {
-      setState(() {
-        _pickImageError = e;
-      });
-    }
-  }
-
-  @override
-  void deactivate() {
-    super.deactivate();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  Widget _previewImages() {
-    final Text? retrieveError = _getRetrieveErrorWidget();
-    if (retrieveError != null) {
-      return retrieveError;
-    }
-    if (_imageFileList != null) {
-      return Semantics(
-          child: ListView.builder(
-            key: UniqueKey(),
-            itemBuilder: (context, index) {
-              return Semantics(
-                label: 'image_picker_example_picked_image',
-                child: kIsWeb
-                    ? Image.network(_imageFileList![index].path)
-                    : Image.file(File(_imageFileList![index].path)),
-              );
-            },
-            itemCount: _imageFileList!.length,
-          ),
-          label: 'image_picker_example_picked_images');
-    } else if (_pickImageError != null) {
-      return Text(
-        'Pick image error: $_pickImageError',
-        textAlign: TextAlign.center,
-      );
-    } else {
-      return const Text(
-        'You have not yet picked an image.3',
-        textAlign: TextAlign.center,
-      );
-    }
-  }
-
-  Future<void> retrieveLostData() async {
-    final LostData response = await _picker.getLostData();
-    if (response.isEmpty) {
-      return;
-    }
-    if (response.file != null) {
-      setState(() {
-        _imageFile = response.file;
-      });
-    } else {
-      _retrieveDataError = response.exception!.code;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title!),
-      ),
-      body: Center(
-        child: !kIsWeb && defaultTargetPlatform == TargetPlatform.android
-            ? FutureBuilder<void>(
-                future: retrieveLostData(),
-                builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.none:
-                    case ConnectionState.waiting:
-                      return const Text(
-                        'You have not yet picked an image.1',
-                        textAlign: TextAlign.center,
-                      );
-                    case ConnectionState.done:
-                      return _previewImages();
-                    default:
-                      if (snapshot.hasError) {
-                        return Text(
-                          'Pick image/video error: ${snapshot.error}}',
-                          textAlign: TextAlign.center,
-                        );
-                      } else {
-                        return const Text(
-                          'You have not yet picked an image.2',
-                          textAlign: TextAlign.center,
-                        );
-                      }
-                  }
-                },
-              )
-            : _previewImages(),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          Semantics(
-            label: 'image_picker_example_from_gallery',
-            child: FloatingActionButton(
-              onPressed: () {
-                _onImageButtonPressed(ImageSource.gallery, context: context);
-              },
-              heroTag: 'image0',
-              tooltip: 'Pick Image from gallery',
-              child: const Icon(Icons.photo),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 16.0),
-            child: FloatingActionButton(
-              onPressed: () {
-                _onImageButtonPressed(ImageSource.camera, context: context);
-              },
-              heroTag: 'image2',
-              tooltip: 'Take a Photo',
-              child: const Icon(Icons.camera_alt),
-            ),
-          ),
-        ],
-      ),
+  ListView _onImageAdd2(PickedFile) {
+    print('===================');
+    return ListView.builder(
+      key: UniqueKey(),
+      itemBuilder: (context, index) {
+        return Semantics(
+          label: 'image_picker_example_picked_image',
+          child: kIsWeb
+              ? Image.network(PickedFile.path)
+              : Image.file(File(PickedFile.path)),
+        );
+      },
+      // itemCount: _imageFileList!.length,
     );
-  }
-
-  Text? _getRetrieveErrorWidget() {
-    if (_retrieveDataError != null) {
-      final Text result = Text(_retrieveDataError!);
-      _retrieveDataError = null;
-      return result;
-    }
-    return null;
   }
 }
