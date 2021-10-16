dfa9ce9b4ca66cd679f77f50a3789d640eb7824b
Devcreators <a41417144@gmail.com>
2021-10-16 15:19:00 +0900

    commit : 캡쳐 및 동영상 촬영 방지

--- codezza/android/app/src/main/kotlin/com/example/codezza/MainActivity.kt
+++ codezza/android/app/src/main/kotlin/com/example/codezza/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.codezza
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import android.view.WindowManager.LayoutParams
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        window.addFlags(LayoutParams.FLAG_SECURE)
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
