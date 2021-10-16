c48896310180474dbf3c979c505bdc17717013a3
bae <wisdom@c21soft.com>
2021-10-16 13:06:00 +0900

    main 수정

--- codezza/lib/main.dart
+++ codezza/lib/main.dart
@@ -1,15 +1,8 @@
-// import 'dart:async';
-// import 'dart:convert';
-
+import 'package:codezza/src/common/CommonModule.dart';
 import 'package:codezza/src/screens/home/HomePage.dart';
 import 'package:codezza/src/screens/auth/LoginPage.dart';
-import 'package:codezza/src/widgets/flutter_session.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-// import 'package:http/http.dart' as http;
-
-// import 'package:codezza/src/screens/PhotoPickExample.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,8 +11,7 @@ void main() async {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       // 로그인 세션 있은 경우 메인 페이지로 없을 경우 로그인 페이지로
-      home:
-          await FlutterSession().get("token") != "" ? HomePage() : LoginPage(),
+      home: (await uid() != "") ? HomePage() : LoginPage(),
     ),
   );
 }
